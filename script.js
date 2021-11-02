c7a8362822ebc6d80085c91b904562edfbbf077b
MyUsername <my@email.com>
2021-11-02 15:16:44 +0100

    bozza livello

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // 1. chiedere all'utente il livello di difficoltà
 // 2. in base alla scelta presa dall'utente creare una griglia con il numero di quadrati prestabilito
 // 3. dare un click che cambia colore ai quadrati, in modo che facciano capire all'utente che il quadrato è stato cliccato
+
+// funzioni 
+
+function generaElemento(elementGen, classPlus) {
+    let node = document.createElement(elementGen);
+    node.classList.add(classPlus);
+    return node
+}
+
+// let difficulty = prompt("Scegli il tuo livello di difficoltà: Basso Medio Alto");
+
+const grill = document.getElementById("container");
+
+for (let i = 0; i < 64; i++) {
+    let newElement = generaElemento("div", "square");
+
+    newElement.addEventListener("click",
+        function() {
+            newElement.classList.add("clicked-active");
+        }
+    );
+
+    grill.appendChild(newElement);
+}
