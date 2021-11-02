a2029dfb24db09cab51ec05cd28f76f8a278a2e6
MyUsername <my@email.com>
2021-11-02 14:44:20 +0100

    inizio

---
+++ script.js
@@ -0,0 +1,3 @@
+// 1. chiedere all'utente il livello di difficoltà
+// 2. in base alla scelta presa dall'utente creare una griglia con il numero di quadrati prestabilito
+// 3. dare un click che cambia colore ai quadrati, in modo che facciano capire all'utente che il quadrato è stato cliccato
